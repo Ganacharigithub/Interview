3c0b8b60155e3ffc7583a092ac629b9ca95091f9
royroque <rroque@provenir.com>
2018-07-20 11:43:06 -0400

    sfsdf

---
+++ interview.java
@@ -0,0 +1,19 @@
+Public class Googlesearch{
+
+public static void main(String args[]){
+Webdriver driver;
+
+string equipment = "Iphone"
+int resultnum=1 billion;
+public void search(String equipment,int resultnum){
+driver.find element(By.id("q")).sendkeys(equipment);
+driver.(keys.enter);
+String resulttext=driver.find element(By.id(resultstaus).get text();
+if(resulttext>=resultnum){
+sys.out.pln("pass");
+}
+
+else{
+sys out("fail);
+}
+}
